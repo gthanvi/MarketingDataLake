791b20fc9a4683df34be3678a54195adb5f4dcca
gthanvi <57343299+gthanvi@users.noreply.github.com>
2019-11-04 14:51:54 +0530

    Bounce view

---
+++ SFMC/Views/vw_Bounce.sql
@@ -0,0 +1,19 @@
+
+Create or Replace View "MARKETING_DB"."SFMC"."vw_Bounce" 
+as 
+SELECT Client_ID
+      ,Send_ID
+      ,Subscriber_Key
+      ,Email_Address
+      ,Subscriber_ID
+      ,List_ID
+      ,Event_Date
+      ,Event_Type
+      ,Bounce_Category
+      ,SMTPCode
+      ,Bounce_Reason
+      ,Batch_ID
+      ,Triggered_Send_External_Key
+      ,_File
+      ,1 aa
+  FROM "MARKETING_DB"."SFMC"."SFMC_ALL" where Event_Type = 'Bounce';
